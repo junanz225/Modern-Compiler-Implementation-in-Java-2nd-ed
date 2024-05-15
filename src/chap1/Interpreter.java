93a71fa0540cfb9836ce76f6d6960ea61595496a
junanzhao <1499563136@qq.com>
2024-05-15 10:09:42 -0700

    Clean up

--- src/chap1/Interpreter.java
+++ src/chap1/Interpreter.java
@@ -3,12 +3,12 @@ package chap1;
 public class Interpreter {
 
     int maxargs(Stm s) {
-        if (s instanceof CompoundStm) {
-            CompoundStm compoundStm = (CompoundStm)s;
+        if (s instanceof CompoundStm compoundStm) {
             return Math.max(maxargs(compoundStm.stm1), maxargs(compoundStm.stm2));
         }
-        else if (s instanceof AssignStm)
+        else if (s instanceof AssignStm) {
             return maxargs(((AssignStm)s).exp);
+        }
         else { // it has to be a PrintStm then
             PrintStm printStm = (PrintStm)s;
             return Math.max(printStm.exps.size(), maxargs(printStm.exps));
@@ -17,10 +17,10 @@ public class Interpreter {
     }
 
     int maxargs(Exp exp) {
-        if (exp instanceof IdExp || exp instanceof NumExp)
+        if (exp instanceof IdExp || exp instanceof NumExp) {
             return 0;
-        else if (exp instanceof OpExp) {
-            OpExp opExp = (OpExp)exp;
+        }
+        else if (exp instanceof OpExp opExp) {
             return Math.max(maxargs(opExp.left), maxargs(opExp.right));
         }
         else { // it has to be an EseqExp then
@@ -30,12 +30,12 @@ public class Interpreter {
     }
 
     int maxargs(ExpList expList) {
-        if (expList instanceof PairExpList) {
-            PairExpList pairExpList = (PairExpList)expList;
+        if (expList instanceof PairExpList pairExpList) {
             return Math.max(maxargs(pairExpList.head), maxargs(pairExpList.tail));
         }
-        else  // it has to be a LastExpList then
+        else  {// it has to be a LastExpList then
             return maxargs(((LastExpList)expList).head);
+        }
     }
 
     Table interpStm(Stm s, Table t) {
